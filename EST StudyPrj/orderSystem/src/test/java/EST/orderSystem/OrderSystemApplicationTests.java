5c28becd47882b0b9ac741a569404215520d6bd0
Autopopcornshooter <70250405+Autopopcornshooter@users.noreply.github.com>
2025-09-19 14:47:56 +0900

    Add: 버전 없는 파일 푸시

---
+++ EST StudyPrj/orderSystem/src/test/java/EST/orderSystem/OrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package EST.orderSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
